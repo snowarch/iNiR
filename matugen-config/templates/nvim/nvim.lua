d3d99d1b2fa30db85d550ef3af47412648e22773
morozoko07-maker <morozoko07@gmail.com>
2026-02-11 18:03:46 +0530

    matugen config for nvim doesnt touch quickshell at all new output source at matugen conf itself

---
+++ matugen-config/templates/nvim/nvim.lua
@@ -0,0 +1,18 @@
+return {
+	base00 = "{{colors.surface.default.hex}}",
+	base01 = "{{colors.surface_container_low.default.hex}}",
+	base02 = "{{colors.surface_container.default.hex}}",
+	base03 = "{{colors.outline.default.hex}}",
+	base04 = "{{colors.outline_variant.default.hex}}",
+	base05 = "{{colors.on_surface.default.hex}}",
+	base06 = "{{colors.on_surface_variant.default.hex}}",
+	base07 = "{{colors.inverse_on_surface.default.hex}}",
+	base08 = "{{colors.error.default.hex}}",
+	base09 = "{{colors.tertiary.default.hex}}",
+	base0A = "{{colors.secondary.default.hex}}",
+	base0B = "{{colors.primary.default.hex}}",
+	base0C = "{{colors.primary_container.default.hex}}",
+	base0D = "{{colors.primary.default.hex}}",
+	base0E = "{{colors.secondary_container.default.hex}}",
+	base0F = "{{colors.error_container.default.hex}}",
+}
